b58f7b6ff5ac1f9e0d564a8a431bd4145d27d047
mcfrojd <mcfrojd@gmail.com>
2025-01-19 00:27:03 +0100

    Pretty formatter

--- sw.js
+++ sw.js
@@ -1,44 +1,44 @@
 const CACHE_NAME = 'high-hand-admin-v2';
 const ASSETS = [
-    'admin.html',
-    'manifest.json',
-    'images/icon-192.png',
-    'images/icon-512.png',
-    'css/styles.css'
+  'admin.html',
+  'manifest.json',
+  'images/icon-192.png',
+  'images/icon-512.png',
+  'css/styles.css'
 ];
 
 self.addEventListener('install', (event) => {
-    console.log('Service Worker: Installing');
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Service Worker: Caching Files');
-                return cache.addAll(ASSETS);
-            })
-            .then(() => self.skipWaiting())
-    );
+  console.log('Service Worker: Installing');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Service Worker: Caching Files');
+        return cache.addAll(ASSETS);
+      })
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', (event) => {
-    console.log('Service Worker: Activated');
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames
-                    .filter(cache => cache !== CACHE_NAME)
-                    .map(cache => {
-                        console.log('Service Worker: Clearing Old Cache');
-                        return caches.delete(cache);
-                    })
-            );
-        })
-    );
+  console.log('Service Worker: Activated');
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(cache => cache !== CACHE_NAME)
+          .map(cache => {
+            console.log('Service Worker: Clearing Old Cache');
+            return caches.delete(cache);
+          })
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-    console.log('Service Worker: Fetching', event.request.url);
-    event.respondWith(
-        fetch(event.request)
-            .catch(() => caches.match(event.request))
-    );
+  console.log('Service Worker: Fetching', event.request.url);
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
+  );
 });
